6b60f4a0454ca49a326af9a85b905aa65f00aacd
Pedro <pedromiguelnevesdemeu@gmail.com>
2021-09-06 22:33:15 -0300

    Tratando navegação com autenticação

---
+++ vuex-e-auth/src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-bytebank">
+    <a class="navbar-brand" href="#">ByteBank</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <navbar-logado v-if="usuarioestaLogado" />
+      <navbar-deslogado v-else />
+      
+    </div>
+  </nav>
+</template>
+
+<script>
+import NavbarDeslogado from './NavbarDeslogado.vue';
+import NavbarLogado from './NavbarLogado.vue';
+export default {
+  components: { NavbarDeslogado, NavbarLogado },
+  
+  computed: {
+    usuarioestaLogado() {
+      return Boolean(localStorage.getItem("token"));
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ vuex-e-auth/src/components/NavbarLogado.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul class="navbar-nav mr-auto">
+    <li class="nav-item">
+      <router-link class="nav-link" to="/">Home</router-link>
+    </li>
+    <li class="nav-item">
+      <router-link to="/gerentes" class="nav-link"> Gerentes </router-link>
+    </li>
+    <li class="nav-item">
+      <router-link to="/cadastre-se" class="nav-link">
+        Cadastre-se
+      </router-link>
+    </li>
+    <li class="nav-item">
+      <a href="#" class="nav-link" @click.prevent="efetuarLogout">Logout</a>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  methods: {
+    efetuarLogout() {
+      localStorage.removeItem("token");
+      this.$router.push({ name: "login" });
+    },
+  },
+};
+</script>
+
+<style>
+</style>
