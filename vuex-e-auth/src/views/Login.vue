cfd67922009fffb5d7a806e4fbd154170c9cf98f
Pedro <pedromiguelnevesdemeu@gmail.com>
2021-09-07 01:56:45 -0300

    msg erro caso login errado

--- vuex-e-auth/src/views/Login.vue
+++ vuex-e-auth/src/views/Login.vue
@@ -10,6 +10,9 @@
         <label for="senha">Senha</label>
         <input type="password" class="form-control" v-model="usuario.senha" />
       </div>
+      <p class="alert alert-danger" v-if="mensagemErro">
+        {{mensagemErro}}
+      </p>
       <button type="submit" class="btn btn-primary brn-block">Logar</button>
       <router-link :to="{ name: 'novo.usuario' }">
         Não possui um cadastro, cadastre-se aqui!
@@ -23,12 +26,21 @@ export default {
   data() {
     return {
       usuario: {},
+      mensagemErro: ''
     };
   },
   methods: {
     efetuarLogin() {
       this.$store.dispatch('efetuarLogin', this.usuario)
-        .then(()=>this.$router.push({ name: 'gerentes'}))
+        .then(()=>{
+          this.$router.push({ name: 'gerentes'})
+          this.mensagemErro = ''
+        })
+        .catch( erro => {
+          if (erro.request.status === 401) {
+            this.mensagemErro = 'Login ou senha inválido(s)'
+          }
+        })
     },
   },
 };
