d2fd4f492ecff3a035b0f72b4ee8721c5ee7636a
Pedro <pedromiguelnevesdemeu@gmail.com>
2021-09-07 00:50:38 -0300

    vuex install - bugfix

---
+++ vuex-e-auth/src/provedor.js
@@ -0,0 +1,14 @@
+import Vuex from 'vuex'
+import Vue from 'vue'
+
+Vue.use(Vuex)
+const estado = {
+    token: null,
+    usuario: {
+
+    }
+}
+
+export default new Vuex.Store({
+    state: estado
+})
